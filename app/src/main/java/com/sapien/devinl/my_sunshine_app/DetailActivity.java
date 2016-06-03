bafa70b7550f71547d9bd4068f482b987ad32c65
Devin Leaman <dleaman212@gmail.com>
2016-06-02 23:05:49 -0700

    Initial commit

---
+++ app/src/main/java/com/sapien/devinl/my_sunshine_app/DetailActivity.java
@@ -0,0 +1,96 @@
+package com.sapien.devinl.my_sunshine_app;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+    ShareActionProvider mShareProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.detail, menu);
+
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+        mShareProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        } else if (id == R.id.action_location) {
+            String location = PreferenceManager.getDefaultSharedPreferences(this)
+                    .getString(getString(R.string.pref_location_key),
+                            getString(R.string.pref_location_default));
+
+            Uri geoData = Uri.parse("geo:0,0?").buildUpon()
+                    .appendQueryParameter("q", location).build();
+
+            Intent locationIntent = new Intent(Intent.ACTION_VIEW);
+            locationIntent.setData(geoData);
+            startActivity(locationIntent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        ShareActionProvider actionProvider = new ShareActionProvider(getActivity());
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+            Intent intent = getActivity().getIntent();
+
+            if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                String forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.detail_text))
+                        .setText(forecast);
+            }
+
+            return rootView;
+        }
+    }
+}
---
+++ app/src/main/java/com/sapien/devinl/my_sunshine_app/MainActivity.java
@@ -0,0 +1,69 @@
+package com.sapien.devinl.my_sunshine_app;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        } else if (id == R.id.action_location) {
+            openPreferredLocationInMap();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationInMap() {
+        String location = PreferenceManager.getDefaultSharedPreferences(this)
+                .getString(getString(R.string.pref_location_key),
+                        getString(R.string.pref_location_default));
+
+        Uri geoData = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location).build();
+
+        Intent locationIntent = new Intent(Intent.ACTION_VIEW);
+        locationIntent.setData(geoData);
+
+        if(locationIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(locationIntent);
+        } else {
+            Log.d("Main_Activity", "Couldn't call " + location);
+        }
+    }
+}
